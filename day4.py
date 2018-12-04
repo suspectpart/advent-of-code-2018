a1c679be3b038ece62820572c10a20e449f2db60
Horst Schneider <horst.schneider@posteo.de>
2018-12-04 23:41:17 +0100

    Refactor

--- day4.py
+++ day4.py
@@ -5,7 +5,7 @@ from collections import defaultdict
 from datetime import datetime
 
 
-class Guard:
+class Guard(object):
     def __init__(self, number):
         self._number = number
         self._minutes = defaultdict(int)
@@ -53,21 +53,26 @@ class Record:
         return int(guard_num.group(1)) if guard_num else None
 
 
+class Guards(dict):
+    def pick(self, number):
+        return self.setdefault(number, Guard(number))
+
+    def sleepiest(self):
+        return sorted(self.values())[-1]
+
+
 if __name__ == '__main__':
     records = [Record(record.strip()) for record in sorted(fileinput.input("inputs/day4.txt"))]
-
-    guards = {}
+    guards = Guards()
     current = None
     start_ = 0
 
     for record in records:
-        if record.begin_shift():
-            who = record.begin_shift()
-            current = guards.setdefault(who, Guard(who))
+        if "#" in record.action():
+            current = guards.pick(record.begin_shift())
         elif "asleep" in record.action():
             start_ = record.minute()
         else:
             current.sleep(start_, record.minute())
 
-    sleeper = sorted(guards.values())[-1]
-    print(sleeper.attack_vector())
+    print(guards.sleepiest().attack_vector())
