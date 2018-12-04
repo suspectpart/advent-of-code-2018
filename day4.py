5e8e2af7e7b3977337bd6201c049485b8df09c9f
Horst Schneider <horst.schneider@posteo.de>
2018-12-04 21:30:42 +0100

    Refactor

--- day4.py
+++ day4.py
@@ -8,7 +8,7 @@ from datetime import datetime
 class Guard:
     def __init__(self, number):
         self._number = number
-        self.felt_asleep = None
+        self._felt_asleep = None
         self._minutes = defaultdict(int)
         self._total = 0
 
@@ -16,13 +16,13 @@ class Guard:
         return self._number
 
     def sleep(self, time_):
-        self.felt_asleep = time_
+        self._felt_asleep = time_
 
     def wake_up(self, time_):
-        for minute in range(self.felt_asleep, time_):
+        for minute in range(self._felt_asleep, time_):
             self._minutes[minute] += 1
 
-        self._total += time_ - self.felt_asleep
+        self._total += time_ - self._felt_asleep
 
     def slept(self):
         return self._total
@@ -39,12 +39,9 @@ class Guard:
     def __lt__(self, other):
         return self.slept() < other.slept()
 
-    def __repr__(self):
-        return f"Guard<num: {self._number}, slept: {self.slept()}, fav: {self.favorite_minute()}>"
-
 
 class Record:
-    pattern = re.compile('\[(?P<date>.+)\]\s(?P<action>.+)')
+    pattern = re.compile('\[(.+)\]\s(.+)')
 
     def __init__(self, string):
         date, action = Record.pattern.match(string).groups()
@@ -55,36 +52,28 @@ class Record:
     def action(self):
         return self._action
 
-    def date(self):
-        return self._date
-
     def minute(self):
         return self._date.minute
 
-    def guard(self):
-        guard_num = re.match("^Guard #(?P<number>\d+)\sbegins shift", self.action())
+    def begin_shift(self):
+        guard_num = re.search("#(\d+)", self.action())
         return int(guard_num.group(1)) if guard_num else None
-
-    def __lt__(self, other):
-        return self.date() < other.date()
-
-    def __repr__(self):
-        return f"Record<date: {self.date()}, action: {self.action()}>"
 
 
 if __name__ == '__main__':
-    records = sorted(Record(record.strip()) for record in fileinput.input("inputs/day4.txt"))
+    records = [Record(record.strip()) for record in sorted(fileinput.input("inputs/day4.txt"))]
 
     guards = {}
     current = None
 
     for record in records:
-        if record.guard():
-            current = guards.setdefault(record.guard(), Guard(record.guard()))
+        if record.begin_shift():
+            who = record.begin_shift()
+            current = guards.setdefault(who, Guard(who))
         elif "asleep" in record.action():
             current.sleep(record.minute())
         else:
             current.wake_up(record.minute())
 
-    sleeper = list(sorted(guards.values(), reverse=True))[0]
+    sleeper = sorted(guards.values())[-1]
     print(sleeper.attack_vector())
